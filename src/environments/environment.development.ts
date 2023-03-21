eed0e8407d2cb3057b7d2f1a3af87c131d4cbaf0
jermia johnson <you@example.com>
2023-03-21 11:23:53 +0530

    Init app

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'angularfinal-b5276',
+    appId: '1:116508747520:web:95529c25e941e1be5135e5',
+    storageBucket: 'angularfinal-b5276.appspot.com',
+    apiKey: 'AIzaSyCPVn-E3VV3fkT75WvU1GjO2TQBpKvyvnA',
+    authDomain: 'angularfinal-b5276.firebaseapp.com',
+    messagingSenderId: '116508747520',
+    measurementId: 'G-804J06BELW',
+  },};
